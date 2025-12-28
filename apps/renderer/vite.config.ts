cb7fd7cf5e7fa4fda82230e1e28bb2bce76221f9
Gabriel Menezes <gabrielsmenezes99@gmail.com>
2025-12-28 19:45:54 -0300

    style: format file

--- apps/renderer/vite.config.ts
+++ apps/renderer/vite.config.ts
@@ -67,6 +67,8 @@ export default defineConfig({
 		target:
 			process.env.TAURI_ENV_PLATFORM === 'windows' ? 'chrome105' : 'safari13',
 		minify: !process.env.TAURI_ENV_DEBUG ? 'esbuild' : false,
-		sourcemap: shouldUploadSourceMaps ? 'hidden' : !!process.env.TAURI_ENV_DEBUG,
+		sourcemap: shouldUploadSourceMaps
+			? 'hidden'
+			: !!process.env.TAURI_ENV_DEBUG,
 	},
 })
