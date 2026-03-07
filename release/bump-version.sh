8b8217f09daf44598e38ef5ebacc8f6b3a95e9bd
Gabriel Menezes <gabrielsmenezes99@gmail.com>
2026-03-07 20:08:17 -0300

    chore(release): allow release from any branch and bump all package versions

--- release/bump-version.sh
+++ release/bump-version.sh
@@ -16,10 +16,14 @@ sed -i "s/^version = \"[^\"]*\"/version = \"$VERSION\"/" Cargo.toml
 # Update tauri.conf.json
 sed -i "s/\"version\": \"[^\"]*\"/\"version\": \"$VERSION\"/" apps/launcher/tauri.conf.json
 
+# Update sub-package versions
+sed -i "s/\"version\": \"[^\"]*\"/\"version\": \"$VERSION\"/" apps/renderer/package.json
+sed -i "s/\"version\": \"[^\"]*\"/\"version\": \"$VERSION\"/" packages/ui/package.json
+
 # Update Cargo.lock
 cargo update -p mrunner -p bookmarks
 
 # Stage the changes
-git add Cargo.toml apps/launcher/tauri.conf.json Cargo.lock
+git add Cargo.toml apps/launcher/tauri.conf.json Cargo.lock apps/renderer/package.json packages/ui/package.json
 
 echo "Version bumped to $VERSION"
